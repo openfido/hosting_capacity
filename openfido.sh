a45461219089650307516ddd50a220270d0ade11
David P. Chassin <dchassin@slac.stanford.edu>
2022-02-16 08:47:15 -0800

    Update openfido.sh

--- openfido.sh
+++ openfido.sh
@@ -21,15 +21,22 @@ if [ ! -f "/usr/local/bin/gridlabd" ]; then
     echo "ERROR [openfido.sh]: '/usr/local/bin/gridlabd' not found" > /dev/stderr
     error
 elif [ ! -f "$OPENFIDO_INPUT/gridlabd.rc" ]; then
-    echo "ERROR [openfido.sh]: '$OPENFIDO_INPUT/gridlabd.rc' not found" > /dev/stderr
-    error
+    OPTIONS=$(ls -1 | tr '\n' ' ')
+    if [ ! -z "$OPTIONS" ]; then
+        echo "WARNING [openfido.sh]: '$OPENFIDO_INPUT/gridlabd.rc' not found, using all input files by default" > /dev/stderr
+    else
+        echo "ERROR [openfido.sh]: no input files"
+        error
+    fi
+else
+    OPTIONS=$(cat gridlabd.rc | tr '\n' ' ')
 fi
 
 echo '*** INPUTS ***'
 ls -l $OPENFIDO_INPUT
 
 TEMPLATE=ica_analysis
-(cd $OPENFIDO_INPUT; gridlabd template get $TEMPLATE ; gridlabd $(tr '\n' ' ' < gridlabd.rc) -t $TEMPLATE ; cp -R . $OPENFIDO_OUTPUT) || error
+(cd $OPENFIDO_INPUT; gridlabd template get $TEMPLATE ; gridlabd $OPTIONS -t $TEMPLATE ; cp -R . $OPENFIDO_OUTPUT) || error
 
 echo '*** OUTPUTS ***'
 ls -l $OPENFIDO_OUTPUT
